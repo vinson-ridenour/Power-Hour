9f7700046e31f3252c0c227ed08efc6654c10848
chesteraustin <chesteraustin@gmail.com>
2017-08-04 15:06:05 -0700

    make signin redirect to dashboard.html

--- power-hour/routes/view-routes.js
+++ power-hour/routes/view-routes.js
@@ -18,8 +18,8 @@ module.exports = function(app) {
   });
 
   // cms route loads cms.html
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  app.get("/dashboard", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/dashboard.html"));
   });
 
   // blog route loads blog.html
