be4965f3fb63e99414fdc0f48451f02e8d26fe79
Vinson Ridenour <vinson.ridenour@gmail.com>
2017-08-04 15:24:24 -0700

    adds most of seq Users model

---
+++ models/user-info.js
@@ -0,0 +1,77 @@
+module.exports = function(sequelize, DataTypes) {
+  var Users = sequelize.define("Users", {
+    // Sets up Users table columns with data types
+    first_name: {
+      type: DataTypes.STRING, // String = 255 characters
+      allowNull: false,
+      validate: {
+        is: /^[a-z]+$/i, // regex - only allows letters
+        len: [1, 30]
+      }
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: /^[a-z]+$/i, // regex - only allows letters
+        len: [1, 30]
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [0, 50]
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [0, 30]
+      }
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [0, 2]
+      }
+    },
+    zip: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [10]
+      }
+    },
+    email_address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isEmail: true, // checks for email format
+        len: [1]
+      }
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [20]
+      }
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  });
+
+  Users.associate = function(models) {
+    // Associating Users with Clients
+    // When a User is deleted, this deletes all their corresponding Clients
+    Users.hasMany(models.Clients, {
+      onDelete: "cascade"
+    });
+  };
+  return Users;
+};
